d7fde1ad0bc923c87354162b9b3ffd486355d5cf
reyren19 <160323190+reyren19@users.noreply.github.com>
2024-12-01 12:37:05 +0000

    Time: 0 ms (100%), Space: 28.5 MB (9.34%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,12 +4,13 @@ public:
         stack<int> s;
         int n = nums.size();
         vector<int> v(n,-1);
+        // run through the array twice
         for(int i=0; i<2*n; i++){
             while(!s.empty() && nums[s.top()]<nums[i%n]){
                 v[s.top()]=nums[i%n];
                 s.pop();
             }
-            if(i<n) s.push(i%n);
+            s.push(i%n);
         }
         return v;
     }
