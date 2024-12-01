158d8aa8da7120231423e9c038ece87e74739ef4
reyren19 <160323190+reyren19@users.noreply.github.com>
2024-12-01 12:38:43 +0000

    Time: 8 ms (41.05%), Space: 28.5 MB (8.19%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,10 +6,13 @@ public:
         vector<int> v(n,-1);
         // run through the array twice
         for(int i=0; i<2*n; i++){
+            /* check if the top of the array is less than the current element, if yes then put the current element as nge 
+            for index at the top */
             while(!s.empty() && nums[s.top()]<nums[i%n]){
                 v[s.top()]=nums[i%n];
                 s.pop();
             }
+            /* push current element as its nge hasnt been found */
             s.push(i%n);
         }
         return v;
