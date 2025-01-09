b50852161b0098c3a71e617ba12f1ee8b9861cfc
Raunak Raj <160323190+reyren19@users.noreply.github.com>
2025-01-10 00:59:07 +0530

    Time: 6 ms (47.13%), Space: 28.5 MB (49.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,16 +3,12 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         stack<int> s;
         int n = nums.size();
-        vector<int> v(n,-1);
-        // run through the array twice
-        for(int i=0; i<2*n; i++){
-            /* check if the top of the array is less than the current element, if yes then put the current element as nge 
-            for index at the top */
+          vector<int> v(n,-1);      
+        for(int i=0; i<n*2; i++){
             while(!s.empty() && nums[s.top()]<nums[i%n]){
                 v[s.top()]=nums[i%n];
                 s.pop();
             }
-            /* push current element as its nge hasnt been found */
             s.push(i%n);
         }
         return v;
