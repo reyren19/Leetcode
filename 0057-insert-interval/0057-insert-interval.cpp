4fe70443b51716f2ea7593ef2dda2557fd1f7ab6
Raunak Raj <160323190+reyren19@users.noreply.github.com>
2025-01-29 00:26:33 +0530

    Time: 3 ms (33.17%), Space: 21.5 MB (94.2%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,12 +4,12 @@ public:
         int n = arr.size();
         vector<vector<int>> ans;
         int i=0;
-        // Loop for insertion of left elements
+        // Loop for insertion of all elements before the newInterval 
         while(i<n && arr[i][1]<newInterval[0]){
             ans.push_back(arr[i]);
             i++;
         }
-        // Loop to merge the overallping elements with new interval
+        // Loop to merge the overallping elements with new interval, see how many intervals newInterval can absorb
         while(i<n && arr[i][0]<=newInterval[1]){
             newInterval[0] = min(arr[i][0], newInterval[0]);
             newInterval[1] = max(arr[i][1], newInterval[1]);
