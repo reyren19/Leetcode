0a76306f6386b687e1669433efa684c91394c781
Raunak Raj <160323190+reyren19@users.noreply.github.com>
2025-01-29 00:41:24 +0530

    Time: 59 ms (39.07%), Space: 93.9 MB (87.24%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& nums) {
-        sort(nums.begin(), nums.end());
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
         int count = 0;
-        int n = nums.size();
-        int prevEnd = nums[0][1];
-        // iterate thru the array and check if the start val of curr is less then end val, if it is cnt++
-
-        for(int i=1;i<n;i++){
-            if(nums[i][0]>=prevEnd){
-                prevEnd = nums[i][1];
+        int prevEnd = intervals[0][1];
+        int i=1;
+        while(i<intervals.size()){
+            if(intervals[i][0]>=prevEnd){
+                prevEnd = intervals[i][1];
             }
-            else {
+            else{
                 count++;
-                prevEnd = min(prevEnd, nums[i][1]);
+                prevEnd = min(prevEnd, intervals[i][1]);
             }
+            i++;
         }
         return count;
-        
     }
 };
