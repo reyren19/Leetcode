2ef1ed2cbfe12f0c6f8863a008322abc6a8bba91
Raunak Raj <160323190+reyren19@users.noreply.github.com>
2025-05-23 06:03:48 +0530

    Time: 4 ms (91.82%), Space: 33.2 MB (61.88%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,10 +1,10 @@
 class Solution {
 private:
-        void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis){
+        void dfs(int i, int j, vector<vector<int>>& grid){
         int n=grid.size();
         int m = grid[0].size();
         // check if the cell isnt invalid
-        if(i<0 || i>=n || j<0 || j>=m || vis[i][j]==1){
+        if(i<0 || i>=n || j<0 || j>=m){
             return;
         }
         if(grid[i][j]!=1){
@@ -13,9 +13,8 @@ private:
         int dR[4]={-1,0,1,0};
         int dC[4]={0,-1,0,1};
         grid[i][j]=2;
-        vis[i][j]=1;
         for(int x=0; x<4; x++){
-            dfs(i+dR[x], j+dC[x], grid, vis);
+            dfs(i+dR[x], j+dC[x], grid);
         }
     }
 public:
@@ -23,21 +22,20 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int cnt=0;
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        // running dfs on all the borders to convert O to Xs
+        // running dfs on all the borders to convert 1 to 2
         for(int i=0; i<n; i++){
             // first cell of each row
-            dfs(i, 0, grid, vis);
+            dfs(i, 0, grid);
             // last cell of each row
-            dfs(i, m-1, grid, vis);
+            dfs(i, m-1, grid);
         }
 
         for(int j=0; j<m; j++){
             // that column cell in the first row
-            dfs(0,j, grid, vis);
+            dfs(0,j, grid);
 
             // that column cell in the last row
-            dfs(n-1, j, grid, vis);
+            dfs(n-1, j, grid);
         }
 
         for(int i=0; i<n; i++){
