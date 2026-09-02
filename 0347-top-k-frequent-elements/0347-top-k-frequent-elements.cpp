2610bf75d478bc91d24a433d1040741d822ee668
Raunak Raj <160323190+reyren19@users.noreply.github.com>
2026-09-02 05:01:08 -0400

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 0 ms (100%), Space: 17.9 MB (73.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>v;
-        unordered_map<int,int>mp;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>mh;
+        vector<int> ans;
+        unordered_map<int,int> freq;
         for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
+            freq[nums[i]]++;
         }
-        for(auto it: mp){
-            mh.push({it.second, it.first});
-            if(mh.size()>k){
-                mh.pop();
+        // make a minheap so we can eliminate the least ones 
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
+        for(auto it: freq){
+            minHeap.push({it.second, it.first}); // since greater<> compares the first of the element first
+            if(minHeap.size()>k){
+                minHeap.pop();
             }
         }
-        while(!mh.empty()){
-            v.push_back(mh.top().second);
-            mh.pop();
+        while(!minHeap.empty()){
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
         }
-        return v;
+        return ans;
     }
 };
